7a59fb41e70e4c40b5f3b5bf6a304f3252e9d95f
zoon199822 <1551927892@qq.com>
2024-05-13 23:39:45 +0800

    更新人物攻击动作

--- script/enemy.gd
+++ script/enemy.gd
@@ -3,10 +3,14 @@ extends CharacterBody2D
 var speed = 30
 var player_chase = false
 var player = null
+var player_back = false
 
 func _physics_process(delta):
 	if player_chase:
-		position += (player.position - position)/speed
+		if player_back:
+			position += -(player.position).normalized() * 10
+		else:
+			position += (player.position - position)/speed
 		$AnimatedSprite2D.play("walk")
 		if(player.position.x - position.x) < 0:
 			$AnimatedSprite2D.flip_h = true
@@ -19,13 +23,20 @@ func _physics_process(delta):
 func _on_detection_aera_body_entered(body):
 	player = body
 	player_chase = true
+	player_back = false
 
 
 func _on_detection_aera_body_exited(body):
 	player = null
 	player_chase = false
 
+
+
+func _on_hurtbox_hurt(hitbox: Hitbox) -> void:
+	stats.health -= 1
+	if stats.health == 0:
+		queue_free()
+@onready var stats: Node = $Stats#2024/5/9 23:01制作血量时添加——Buryn
 #连接节点触发之后 史莱姆碰到玩家会被击退
-#func _on_hitbox_area_entered(area):
-	#player = area
-	#player_chase = true
+func _on_hitbox_area_entered(area):
+	pass
--- script/player.gd
+++ script/player.gd
@@ -7,26 +7,61 @@ const friction = 600
 
 var velocity2 = Vector2.ZERO
 
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 @onready var state_machine = $AnimationTree.get("parameters/playback")
 
 func _ready():
 	$AnimationTree.active = true
 	$AnimationTree.set("parameters/idel/blend_position", Vector2.DOWN)
-#玩家行动逻辑
+
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ROLL:
+			pass
+		
+		ATTACK:
+			attack_state(delta)
+			
+#玩家行动逻辑
+func move_state(delta):
 	var input_vector = Vector2.ZERO
-	
 	input_vector.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
 	input_vector.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity2 = velocity2.move_toward(input_vector * max_speed, accel * delta)
 		$AnimationTree.set("parameters/idel/blend_position", input_vector)
 		$AnimationTree.set("parameters/move/blend_position", input_vector)
+		$AnimationTree.set("parameters/attack/blend_position", input_vector)
 		state_machine.travel("move")
+		velocity2 = velocity2.move_toward(input_vector * max_speed, accel * delta)
 	else:
 		velocity2 = velocity2.move_toward(Vector2.ZERO, friction * delta)
 		state_machine.travel("idel")
 		
 	move_and_collide(velocity2 * delta)
+	
+	#攻击
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	
+	
+#攻击
+func attack_state(delta):
+	state_machine.travel("attack")
+	
+func attack_animation_finished():
+	state = MOVE
+	
+func roll_state():
+	pass
